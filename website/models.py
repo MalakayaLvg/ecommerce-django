4f7ebf25c7dbef7dcd95ea451cd11b077ae1b24c
Oslynx <malakayalvg@gmail.com>
2024-11-05 18:02:08 +0100

    product image

--- website/models.py
+++ website/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     rate = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
+    image = models.ImageField(upload_to='products/',null=True)
 
 class Comment(models.Model):
     content = models.TextField()
--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -10,3 +12,6 @@ urlpatterns = [
     path("product/<int:product_id>/delete/", views.comment_delete, name="comment_delete"),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
